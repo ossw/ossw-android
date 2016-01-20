3a5f49b8b94ce4c9f19c8ecdefd47c244d76bd12
Pavel Vasilyev <pkvasilyev@gmail.com>
2016-01-20 01:00:16 +0100

    Add Polish translation, some cleaning

--- app/src/main/java/com/althink/android/ossw/service/ble/BleConnectionStatus.java
+++ app/src/main/java/com/althink/android/ossw/service/ble/BleConnectionStatus.java
@@ -1,8 +1,25 @@
 package com.althink.android.ossw.service.ble;
 
+import com.althink.android.ossw.R;
+import com.althink.android.ossw.service.OsswService;
+
 /**
  * Created by krzysiek on 10/10/15.
+ * vaspa: Add resource id for internationalization
  */
 public enum BleConnectionStatus {
-    DISCONNECTED, CONNECTING, CONNECTED, AUTO_RECONNECT
+    DISCONNECTED(R.string.disconnected),
+    CONNECTING(R.string.connecting),
+    CONNECTED(R.string.connected),
+    AUTO_RECONNECT(R.string.auto_reconnect);
+    private int stringId;
+
+    BleConnectionStatus(int sId) {
+        stringId = sId;
+    }
+
+    @Override
+    public String toString() {
+        return OsswService.getInstance().getApplicationContext().getString(stringId);
+    }
 }
